571ad611c7776728e2a02b610aac74ec8585edd0
Omar Naffaa <37256494+OmarNaffaa@users.noreply.github.com>
2020-09-29 17:01:42 -0700

    Testbench added

---
+++ FIFO_TB.sv
@@ -0,0 +1,257 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: Cal Poly Pomona
+// Engineer: Omar Naffaa
+//////////////////////////////////////////////////////////////////////////////////
+
+module FIFO_TB();
+
+    // signal declarations
+    localparam DATA_WIDTH = 4;
+    localparam ADDR_WIDTH = 2;
+    localparam T = 10; //clock period
+    
+    logic clk, reset;
+    logic wr, rd;
+    logic full, empty;
+    logic [DATA_WIDTH - 1: 0] r_data;
+    logic [2 * DATA_WIDTH - 1: 0] w_data;
+    
+    // instantiate module under test
+    FIFO #(.DATA_WIDTH(DATA_WIDTH), .ADDR_WIDTH(ADDR_WIDTH)) uut (.*);
+    
+    // 10 ns clock running forever
+    always
+    begin
+        clk = 1'b1;
+        #(T / 2);
+        clk = 1'b0;
+        #(T / 2);
+    end
+    
+    // reset for the first half cylce
+    initial
+    begin
+        reset = 1'b1;
+        rd = 1'b0;
+        wr = 1'b0;
+        @(negedge clk);
+        reset = 1'b0;
+    end
+    
+    //test vectors
+    initial
+    begin
+        // ----------------EMPTY-----------------------
+        // write
+        @(negedge clk);
+        w_data = 1;
+        wr = 1'b1;     
+        @(negedge clk);
+        wr = 1'b0;
+        
+        // write
+        repeat(1) @(negedge clk);
+        w_data = 2;
+        wr = 1'b1;
+        @(negedge clk)
+        wr = 1'b0;
+        
+        // write
+        repeat(1) @(negedge clk);
+        w_data = 3;
+        wr = 1'b1;
+        @(negedge clk)
+        wr = 1'b0;              
+        
+        // write
+        repeat(1) @(negedge clk);
+        w_data = 4;
+        wr = 1'b1;
+        @(negedge clk)
+        wr = 1'b0;        
+        
+        // write
+        repeat(1) @(negedge clk);
+        w_data = 5;
+        wr = 1'b1;
+        @(negedge clk)
+        wr = 1'b0;               
+        
+        // ----------------FULL-----------------------
+        // read
+        repeat(1) @(negedge clk);
+        rd = 1'b1;
+        @(negedge clk)
+        rd = 1'b0;
+        
+        // read
+        repeat(1) @(negedge clk);
+        rd = 1'b1;
+        @(negedge clk)
+        rd = 1'b0;
+        
+        // read
+        repeat(1) @(negedge clk);
+        rd = 1'b1;
+        @(negedge clk)
+        rd = 1'b0;
+        
+        // read
+        repeat(1) @(negedge clk);
+        rd = 1'b1;
+        @(negedge clk)
+        rd = 1'b0;
+        
+        // read
+        repeat(1) @(negedge clk);
+        rd = 1'b1;
+        @(negedge clk)
+        rd = 1'b0;
+        
+        // read
+        repeat(1) @(negedge clk);
+        rd = 1'b1;
+        @(negedge clk)
+        rd = 1'b0;
+        
+        // read
+        repeat(1) @(negedge clk);
+        rd = 1'b1;
+        @(negedge clk)
+        rd = 1'b0;
+        
+        // read
+        repeat(1) @(negedge clk);
+        rd = 1'b1;
+        @(negedge clk)
+        rd = 1'b0;
+        
+         // read
+        repeat(1) @(negedge clk);
+        rd = 1'b1;
+        @(negedge clk)
+        rd = 1'b0;
+        
+        // ----------------EMPTY-----------------------
+        
+        // read & write at the same time
+        repeat(1) @(negedge clk);
+        w_data = 16'h9889;
+        wr = 1'b1;
+        rd = 1'b1;
+        @(negedge clk)
+        wr = 1'b0;
+        rd = 1'b0;
+        
+        // read & write at the same time
+        repeat(1) @(negedge clk);
+        w_data = 16'h8778;
+        wr = 1'b1;
+        rd = 1'b1;
+        @(negedge clk)
+        wr = 1'b0;
+        rd = 1'b0;
+        
+        // read & write at the same time
+        repeat(1) @(negedge clk);
+        w_data = 16'h7667;
+        wr = 1'b1;
+        rd = 1'b1;
+        @(negedge clk)
+        wr = 1'b0;
+        rd = 1'b0;
+        
+        // read & write at the same time
+        repeat(1) @(negedge clk);
+        w_data = 16'h6556;
+        wr = 1'b1;
+        rd = 1'b1;
+        @(negedge clk)
+        wr = 1'b0;
+        rd = 1'b0;
+        
+        // read & write at the same time
+        repeat(1) @(negedge clk);
+        w_data = 16'h5445;
+        wr = 1'b1;
+        rd = 1'b1;
+        @(negedge clk)
+        wr = 1'b0;
+        rd = 1'b0;
+        
+        // read 
+        repeat(1) @(negedge clk);
+        rd = 1'b1;
+        @(negedge clk)
+        rd = 1'b0;
+        
+        // read 
+        repeat(1) @(negedge clk);
+        rd = 1'b1;
+        @(negedge clk)
+        rd = 1'b0;
+
+        // ----------------NOT EMPTY-----------------------
+        repeat(1) @(negedge clk);
+        w_data = 16'h8008;
+        wr = 1'b1;
+        @(negedge clk)
+        wr = 1'b0;
+        
+        // read & write at the same time
+        repeat(1) @(negedge clk);
+        w_data = 16'h0220;
+        wr = 1'b1;
+        rd = 1'b1;
+        @(negedge clk)
+        wr = 1'b0;
+        rd = 1'b0;
+        
+        // read
+        repeat(1) @(negedge clk);
+        rd = 1'b1;
+        @(negedge clk)
+        rd = 1'b0;
+        
+        // read
+        repeat(1) @(negedge clk);
+        rd = 1'b1;
+        @(negedge clk)
+        rd = 1'b0;
+        
+        // read
+        repeat(1) @(negedge clk);
+        rd = 1'b1;
+        @(negedge clk)
+        rd = 1'b0;
+        
+        // read
+        repeat(1) @(negedge clk);
+        rd = 1'b1;
+        @(negedge clk)
+        rd = 1'b0;
+        
+        // read
+        repeat(1) @(negedge clk);
+        rd = 1'b1;
+        @(negedge clk)
+        rd = 1'b0;
+        
+        // read
+        repeat(1) @(negedge clk);
+        rd = 1'b1;
+        @(negedge clk)
+        rd = 1'b0;
+        
+        // read
+        repeat(1) @(negedge clk);
+        rd = 1'b1;
+        @(negedge clk)
+        rd = 1'b0;
+        
+        repeat(3) @(negedge clk);
+        $stop;
+    end
+
+endmodule
